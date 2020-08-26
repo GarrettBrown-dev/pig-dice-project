5671556bf429f6342710859498d10175c5bc4de4
Janet Karpenske <janetkarpenske@gmail.com>
2020-08-26 11:37:17 -0700

    Add constructor functions to create and store 2 players

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,48 @@
-// Business Logic -------
+// let player1 = {score: 0,
+//     active: true}; 
+// let player2 = {score: 0,
+//     active: false};
+
+
+
+
+
+// Player Business Logic --------
+
 function Players() {
   this.players = [];
 }
 
-
-Players.prototype.addPlayer = function(player) {
-  this.players.push(player)
+Players.prototype.addPlayer = function (player) {
+  this.players.push(player);
 }
 
-// Dice Roll Logic -------
+// Players Business Logic --------
 
+function Player(totalScore, turnScore, active) {
+  this.totalScore = totalScore;
+  this.turnScore = turnScore;
+  this.active = active;
+}
+
+// Player Change Logic
+
+// function changePlayer() {
+//   if 
+// }
+// Score Logic ---------
+
+function turnScore(diceRoll) {
+  if (diceRoll === 1) {
+    changePlayer;
+    return "false" + (player1.turnScore = 0);
+  }
+  else {
+    return this.player1 += diceRoll;
+  }
+}
+
+// Dice Roll Logic ---------
 let dice = {
   sides: 6,
   roll: function () {
@@ -19,15 +52,31 @@ let dice = {
 }
 
 // User Interface Logic ---------
-let players = new Players();
 
-$(document).ready(function() {
-  let player1 = 0;
-  let player2 = 0;
+$(document).ready(function () {
+  let players = new Players();
+  let player1 = new Player(0, 0, true);
+  let player2 = new Player(0, 0, false);
   players.addPlayer(player1);
   players.addPlayer(player2);
-  console.log(players);
   $("button#roll-btn").click(function () {
-    console.log(dice.roll());
+    let diceRoll = dice.roll();
+    console.log("rolled: " + diceRoll);
+    console.log(players.activePlayer);
+    console.log(players[2]);
   });
 });
+
+// let players = new Players();
+
+// $(document).ready(function() {
+//   let player1 = 0;
+//   let player2 = 0;
+//   players.addPlayer(player1);
+//   players.addPlayer(player2);
+//   console.log(players);
+//   $("button#roll-btn").click(function () {
+//     let diceRoll = dice.roll();
+//     console.log(players.turnScore(diceRoll));
+//   });
+// });
